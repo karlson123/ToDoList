c42cd92040566527a42c1aab6e0a7ed2297d4cd1
Dima Pi <dimapi@dp-mba.local>
2018-11-03 23:00:12 +0200

    convert to MVC design pattern

--- ToDoList/Controllers/ToDoListViewController.swift
+++ ToDoList/Controllers/ToDoListViewController.swift
@@ -10,16 +10,31 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-    var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogrgon"]
+  //  var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogrgon"]
+    var itemArray = [Item]()
     let defaults = UserDefaults.standard     //user default file - location can see from AppDelegate.swift
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let items = defaults.array(forKey: "ToDoListArray") as? [String] {
-            
-            
-        itemArray = items
+        
+        let newItem = Item()
+        newItem.title = "Find Mike"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Buy Eggos"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Destroy Demogorgon"
+        itemArray.append(newItem3)
+        
+       
+        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
+            itemArray = items
     }
+  
+    
     }
 //start count how many items in itemArray, and output this number from this function
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -29,30 +44,32 @@ class ToDoListViewController: UITableViewController {
 
 //start display all items from itemArray into table that was created in func above
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
         let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-        cell.textLabel?.text = itemArray[indexPath.row]
+        let item = itemArray[indexPath.row]
+        cell.textLabel?.text = item.title
         
+        
+        //value = condition ? value if true : value if false
+        //cell.accessoryType = item.done == true ? .checkmark : .none
+        //or more shorter
+        cell.accessoryType = item.done ? .checkmark : .none
+//        //line above is same as the if below
+//        if item.done == true {
+//            cell.accessoryType = .checkmark
+//        } else {
+//            cell.accessoryType = .none
+//        }
         return cell
     }
 //end  display all items from itemArray into table that was created in func above
 
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // print (itemArray[indexPath.row])
+//! mean oposite, if left side not equal to right side true or false
+        itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+     
+        tableView.reloadData()
 
-
-        //check if row has checkmark than add or remove it
-        if tableView.cellForRow(at: indexPath)?.accessoryType ==  .checkmark {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .none
-        } else {
-            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-        }
-
-    
-	    
-    
-    
         tableView.deselectRow(at: indexPath, animated: true)  //make selecet row more nice visual
     }
   
@@ -97,12 +114,13 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen when user clik the add item button
          
-                self.itemArray.append(textField.text!) //add new item to itemArray
-          
-            self.defaults.set(self.itemArray, forKey: "ToDoListArray")     //all items from itemarray put into defaults file
             
-            print(self.defaults.array(forKey: "ToDoListArray")!)
+            let newItem = Item()
+            newItem.title = textField.text!
+                self.itemArray.append(newItem) //add new item to itemArray
             
+                self.defaults.set(self.itemArray, forKey: "ToDoListArray")     //all items from itemarray put into defaults file
+
             self.tableView.reloadData() //reload table with new item
             
             
