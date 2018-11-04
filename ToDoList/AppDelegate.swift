a8bbd1669774baa54592f62bb93a039a17c3d730
Dima Pi <dimapi@dp-mba.local>
2018-11-04 11:16:23 +0200

    persisting data using codable and plist

--- ToDoList/AppDelegate.swift
+++ ToDoList/AppDelegate.swift
@@ -17,10 +17,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        print("didFinishLaunchingWithOptions")
+    
         
-        //path for defaults path file
-        print(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true).last! as String)
         
         return true
     }
--- ToDoList/Controllers/ToDoListViewController.swift
+++ ToDoList/Controllers/ToDoListViewController.swift
@@ -10,30 +10,20 @@ import UIKit
 
 class ToDoListViewController: UITableViewController {
     
-  //  var itemArray = ["Find Mike", "Buy Eggos", "Destroy Demogrgon"]
     var itemArray = [Item]()
-    let defaults = UserDefaults.standard     //user default file - location can see from AppDelegate.swift
+    let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+
+        print(dataFilePath)
         
-        let newItem = Item()
-        newItem.title = "Find Mike"
-        itemArray.append(newItem)
-        
-        let newItem2 = Item()
-        newItem2.title = "Buy Eggos"
-        itemArray.append(newItem2)
-        
-        let newItem3 = Item()
-        newItem3.title = "Destroy Demogorgon"
-        itemArray.append(newItem3)
+    
         
        
-        if let items = defaults.array(forKey: "ToDoListArray") as? [Item] {
-            itemArray = items
-    }
-  
+loadItems()
+  	
     
     }
 //start count how many items in itemArray, and output this number from this function
@@ -68,44 +58,14 @@ class ToDoListViewController: UITableViewController {
 //! mean oposite, if left side not equal to right side true or false
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
      
-        tableView.reloadData()
+        
+        saveItems()
 
         tableView.deselectRow(at: indexPath, animated: true)  //make selecet row more nice visual
     }
   
     
-   /*swipe right
-    override func tableView(_ tableView: UITableView,
-                   leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
-    {
-        let closeAction = UIContextualAction(style: .normal, title:  "Close", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
-            print("OK, marked as Closed")
-            success(true)
-        })
-        closeAction.image = UIImage(named: "tick")
-        closeAction.backgroundColor = .purple
-        
-        return UISwipeActionsConfiguration(actions: [closeAction])
-        
-    }
-    */
-    
-    /* swipe left
-    override func tableView(_ tableView: UITableView,
-                   trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
-    {
-        let modifyAction = UIContextualAction(style: .normal, title:  "Update", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
-            print("Update action ...")
-            success(true)
-        })
-        modifyAction.image = UIImage(named: "hammer")
-        modifyAction.backgroundColor = .blue
-        
-        return UISwipeActionsConfiguration(actions: [modifyAction])
-    }
-    */
-    
-    //add new item + sign top right
+  
 
 
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
@@ -114,23 +74,12 @@ class ToDoListViewController: UITableViewController {
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
             //what will happen when user clik the add item button
          
-            
             let newItem = Item()
             newItem.title = textField.text!
                 self.itemArray.append(newItem) //add new item to itemArray
             
-                self.defaults.set(self.itemArray, forKey: "ToDoListArray")     //all items from itemarray put into defaults file
-
-            self.tableView.reloadData() //reload table with new item
-            
-            
-        /* check if item already in the list and pop up of added item
-            if self.itemArray.contains(textField.text!) == true{
-                print("already contain")
-                let alert = UIAlertController(title: "", message: "\(textField.text!) - added", preferredStyle: .alert)
-                alert.addAction(UIAlertAction(title: "Done", style: UIAlertAction.Style.default, handler: nil))
-                self.present(alert, animated: true, completion: nil)
-            } */
+          self.saveItems()
+      
         }
         
         alert.addTextField { (alertTextField) in
@@ -150,18 +99,73 @@ class ToDoListViewController: UITableViewController {
     
 
 
+    func saveItems() {
+    let encoder = PropertyListEncoder()
+    
+    do {
+    let data = try encoder.encode(itemArray)
+    try data.write(to: dataFilePath!)
+    } catch {
+    print("error encoding itemarray \(error)")
+    }
+    self.tableView.reloadData() //reload table with new item
+}
 
+    func loadItems() {
+        if let data = try? Data(contentsOf: dataFilePath!) {
+            let decoder = PropertyListDecoder()
+            do {
+             itemArray = try decoder.decode([Item].self, from: data)
+            } catch {
+                print("error of decode data \(error)")
+                
+            }
+
+        }
+    }
 
 
 
 }
 
 
+/* check if item already in the list and pop up of added item
+ if self.itemArray.contains(textField.text!) == true{
+ print("already contain")
+ let alert = UIAlertController(title: "", message: "\(textField.text!) - added", preferredStyle: .alert)
+ alert.addAction(UIAlertAction(title: "Done", style: UIAlertAction.Style.default, handler: nil))
+ self.present(alert, animated: true, completion: nil)
+ } */
 
+/*swipe right
+ override func tableView(_ tableView: UITableView,
+ leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
+ {
+ let closeAction = UIContextualAction(style: .normal, title:  "Close", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
+ print("OK, marked as Closed")
+ success(true)
+ })
+ closeAction.image = UIImage(named: "tick")
+ closeAction.backgroundColor = .purple
+ 
+ return UISwipeActionsConfiguration(actions: [closeAction])
+ 
+ }
+ */
 
+/* swipe left
+ override func tableView(_ tableView: UITableView,
+ trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration?
+ {
+ let modifyAction = UIContextualAction(style: .normal, title:  "Update", handler: { (ac:UIContextualAction, view:UIView, success:(Bool) -> Void) in
+ print("Update action ...")
+ success(true)
+ })
+ modifyAction.image = UIImage(named: "hammer")
+ modifyAction.backgroundColor = .blue
+ 
+ return UISwipeActionsConfiguration(actions: [modifyAction])
+ }
+ */
 
-
-
-
-
-
+//add new item + sign top right
